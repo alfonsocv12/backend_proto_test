b879cb983c20974db5fd9c1ee132025ab8edd1db
alfonsocv12 <alfonsocvu@gmail.com>
2023-02-16 17:07:42 -0600

    Add cors

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
 from src.incoming.players.http_handler import router as player_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(player_router, tags=['players'])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 if __name__ == '__main__':
     uvicorn.run(
