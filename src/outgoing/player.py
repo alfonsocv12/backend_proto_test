ccb225a31b3edbc28525b2684e12882d57336965
alfonsocv12 <alfonsocvu@gmail.com>
2023-02-16 19:40:09 -0600

    Adding time to proto

--- src/outgoing/player.py
+++ src/outgoing/player.py
@@ -1,21 +1,26 @@
+from datetime import datetime
+
 player_list: list[dict] = [
     {
         'id': 'd20f83b1-29b0-4d24-81bf-7d06000395f1',
         'name': 'Sophi',
         'height': 1.70,
-        'country_id': '1'
+        'country_id': '1',
+        'created_at': datetime.now()
     },
     {
         'id': '9dd844d3-9059-4b0a-affe-20b12f29e3d0',
         'name': 'Alfonso',
         'height': 1.80,
-        'country_id': '1'
+        'country_id': '1',
+        'created_at': datetime.now()
     },
     {
         'id': '34f18fb4-f1ab-4838-bccf-ce8a20ff2864',
         'name': 'Boby',
         'height': 1.70,
-        'country_id': '1'
+        'country_id': '1',
+        'created_at': datetime.now()
     }
 ]
 
--- src/service/player.py
+++ src/service/player.py
@@ -1,18 +1,23 @@
 import json
-import src.domain.player_pb2 as player_pb2
+from  src.domain.player_pb2 import PlayerEntity
 from src.outgoing.player import PlayerOutgoing
 from google.protobuf import json_format
+from google.protobuf.timestamp_pb2 import Timestamp
 
 class PlayerService:
     player_outgoing = PlayerOutgoing()
     
-    def get_player(self) -> list[player_pb2.PlayerEntity]:
+    def get_player(self) -> list[PlayerEntity]:
         players_raw = self.player_outgoing.get_players()
         
         players = []
         for player in players_raw:
-            player_entity = player_pb2.PlayerEntity()
-            json_format.Parse(json.dumps(player), player_entity)
+            player_entity = PlayerEntity()
+            player_entity.id = player['id']
+            player_entity.name = player['name']
+            player_entity.height = player['height']
+            player_entity.country_id = player['country_id']
+            player_entity.created_at.FromDatetime(player['created_at'])
             players.append(player_entity)
             
         return players
